36b66fdca4934280dee3b736fb2d9e22d10ed459
KristiGordon <KristiMarieGordon@gmail.com>
2022-07-29 20:35:23 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#for i in range(100, 1, -1):
+    #print(f"{i} bottles of beer on the wall.\n{i} bottles of beer.\nTake one down, pass it around, {i} bottles of beer on the wall")
+    #print("*****************************************")
+
+num = 100
+while num > 0:
+    print(f"{num} bottles of beer on the wall.\n{num} bottles of beer.\nTake one down, pass it around, {num} bottles of beer on the wall")
+    num -= 1 
